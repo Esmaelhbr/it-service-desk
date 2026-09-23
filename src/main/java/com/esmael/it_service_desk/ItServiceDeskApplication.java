742906d6bd84228283939b392d58629f3890c9a8
Esmaelhbr <esmael7@live.com>
2026-09-22 23:34:14 -0400

    commit first application files

---
+++ src/main/java/com/esmael/it_service_desk/ItServiceDeskApplication.java
@@ -0,0 +1,13 @@
+package com.esmael.it_service_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItServiceDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItServiceDeskApplication.class, args);
+	}
+
+}
